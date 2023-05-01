b7408913549f3ac6d779942377f35e4905cbc33f
syoung0 <affogato128@gmail.com>
2023-05-01 15:15:13 +0900

    create owner_domain

---
+++ src/main/java/ssginc_kdt_team3/BE/DTOs/owner/ownerDTO.java
@@ -0,0 +1,17 @@
+package ssginc_kdt_team3.BE.DTOs.owner;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@ToString
+public class ownerDTO {
+
+
+
+
+}
