86613036e81cd37bfbae54885307ab4a71d98c21
syoung0 <affogato128@gmail.com>
2023-05-01 15:23:55 +0900

    ?

---
+++ src/main/java/ssginc_kdt_team3/BE/domain/Owner.java
@@ -0,0 +1,57 @@
+package ssginc_kdt_team3.BE.domain;
+
+import com.sun.istack.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "owner")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Owner{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull
+    @Column(name = "owner_email" , length = 50)
+    private String email;
+
+    @NotNull
+    @Column(name = "owner_password",length = 20)
+    private String password;
+
+    @NotNull
+    @Column(name = "owner_name",length = 10)
+    private String name;
+
+    @NotNull
+    @Column(name = "owner_phone" , length = 20)
+    private String phone;
+
+    @Column(name="owner_birthday")
+    private LocalDate birthday;
+
+    @Column(name = "owner_gender")
+    private boolean gender;
+
+    @Column(name="owner_address")
+    private String address;
+
+    @Column(name="owner_role")
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "owner_status")
+    private UserStatus status;
+
+}
