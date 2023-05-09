2531b4825eb319ec3dc9503c12d44605d75a75f3
imtaegyeong <xorud1305@gmail.com>
2023-05-09 16:59:15 +0900

    add show customer reservation list

--- src/main/java/ssginc_kdt_team3/BE/repository/reservation/JpaDataReservationRepository.java
+++ src/main/java/ssginc_kdt_team3/BE/repository/reservation/JpaDataReservationRepository.java
@@ -1,10 +1,23 @@
 package ssginc_kdt_team3.BE.repository.reservation;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.parameters.P;
 import ssginc_kdt_team3.BE.domain.Customer;
 import ssginc_kdt_team3.BE.domain.Reservation;
+import ssginc_kdt_team3.BE.enums.ReservationStatus;
+
+import java.util.List;
 
 public interface JpaDataReservationRepository extends JpaRepository<Reservation, Long> {
 
+    @Query("select r from Reservation r where r.customer.id = :customerId and r.status = :status")
+    List<Reservation> findAllActive(@Param("customerId") Long customerId, @Param("status")ReservationStatus status);
+
+    @Query("select r from Reservation r where r.customer.id = :customerId")
+    List<Reservation> findAllMy(@Param("customerId") Long customerId);
 }
