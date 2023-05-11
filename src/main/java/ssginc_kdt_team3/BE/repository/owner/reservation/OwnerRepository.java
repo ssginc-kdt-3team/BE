d88bc674bfe7bf61366d6560eaa092e3702c1480
IHyeonii <dlgusdllll@gmail.com>
2023-05-11 19:51:50 +0900

    temp

--- src/main/java/ssginc_kdt_team3/BE/repository/owner/reservation/OwnerRepository.java
+++ src/main/java/ssginc_kdt_team3/BE/repository/owner/reservation/OwnerRepository.java
@@ -2,12 +2,10 @@ package ssginc_kdt_team3.BE.repository.owner.reservation;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
-import ssginc_kdt_team3.BE.DTOs.reservation.OwnerReserveDTO;
-import ssginc_kdt_team3.BE.domain.Customer;
 import ssginc_kdt_team3.BE.domain.Reservation;
+import ssginc_kdt_team3.BE.enums.ReservationStatus;
 
 import javax.persistence.EntityManager;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -33,21 +31,22 @@ public class OwnerRepository {
   public void updateReserve(Long id, Reservation reserve){
   }
 
-  // 매장 모든 예약내역 조회, 지점정보랑 shop id가 있어야 되나 ?
+  // 매장 모든 예약내역 조회: 지점정보랑 shop id가 있어야 되나 ?
   public List<Reservation> findAllReserve(){
     List<Reservation> resultReserve = em.createQuery("SELECT r FROM Reservation r", Reservation.class)
         .getResultList();
     return resultReserve;
   }
 
-  // 당일 예약 시간별 조회
-//  public Optional<Reservation> findReserveTime(LocalDateTime reservationDate){
-//    return em.createQuery("SELECT r FROM Reservation r WHERE r.reservationDate = :reservationDate", Reservation.class)
-//        .setParameter("reservationDate", reservationDate)
-//        .getResultList()
-//        .stream()
-//        .findAny();
-//  }
+  // 활성화된 예약 조회
+  public List<Reservation> findByStatus(ReservationStatus status){
+    return em.createQuery("SELECT r FROM Reservation r WHERE r.status = :status", Reservation.class)
+        .setParameter("status", ReservationStatus.RESERVATION)
+        .getResultList();
+  }
 
-
-}
+  // 당일예약 시간별 조회
+  public List<Reservation> findDateAfter(LocalDateTime dateTime){
+    return em.createQuery("SELECT r FROM Reservation r", Reservation.class)
+        .getResultList();
+  }
