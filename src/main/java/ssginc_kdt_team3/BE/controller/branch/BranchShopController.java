c5430edb5ff848f1effc31a4c0addfe32fb214b0
imtaegyeong <xorud1305@gmail.com>
2023-05-17 21:36:25 +0900

    replace shop detail

--- src/main/java/ssginc_kdt_team3/BE/controller/branch/BranchShopController.java
+++ src/main/java/ssginc_kdt_team3/BE/controller/branch/BranchShopController.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ssginc_kdt_team3.BE.DTOs.shop.ShopDetailDTO;
 import ssginc_kdt_team3.BE.domain.Review;
 import ssginc_kdt_team3.BE.service.Shop.ShopDetailService;
 import ssginc_kdt_team3.BE.service.admin.branch.BranchShopListService;
@@ -18,7 +19,7 @@ public class BranchShopController {
 
     private final ShopDetailService detailService;
 
-    @GetMapping("/list")
+    @GetMapping("/list/")
     public ResponseEntity<String> branchShopList(@RequestParam("id") long id) throws Exception {
         try {
             String branchShopJSON = branchshop.BranchShop(id);
@@ -30,13 +31,20 @@ public class BranchShopController {
     }
 
     @GetMapping("/detail/{id}")
-    public ResponseEntity<String> ShopDetail(@PathVariable("id") Long id) throws Exception {
-        try {
-            String shopDetail = detailService.ShopDetailList(id);
-            return new ResponseEntity<>(shopDetail, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ShopDetailDTO> ShopDetail(@PathVariable("id") Long id) throws Exception {
+        ShopDetailDTO shopDetailDTO = detailService.ShopDetailList(id);
+
+        if (shopDetailDTO != null) {
+            return ResponseEntity.ok(shopDetailDTO);
         }
+        return ResponseEntity.badRequest().build();
+
+//        try {
+//            String shopDetail = (String) shopDetailDTO;
+//            return new ResponseEntity<>(shopDetail, HttpStatus.OK);
+//        } catch (Exception e) {
+//            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+//        }
     }
 //    @GetMapping("/review/{id}/{id}")
 //    public ResponseEntity<Page<Review>> PageReview(@PathVariable("id"))
