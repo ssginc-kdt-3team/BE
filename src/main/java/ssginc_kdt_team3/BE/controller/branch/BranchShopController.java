9092e1bf9bbfebf167566ff5813ce99bd02da9b2
syoung0 <affogato128@gmail.com>
2023-05-17 20:08:12 +0900

    ShopDetail

--- src/main/java/ssginc_kdt_team3/BE/controller/branch/BranchShopController.java
+++ src/main/java/ssginc_kdt_team3/BE/controller/branch/BranchShopController.java
@@ -1,9 +1,11 @@
 package ssginc_kdt_team3.BE.controller.branch;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ssginc_kdt_team3.BE.domain.Review;
 import ssginc_kdt_team3.BE.service.Shop.ShopDetailService;
 import ssginc_kdt_team3.BE.service.admin.branch.BranchShopListService;
 
@@ -30,12 +32,15 @@ public class BranchShopController {
     @GetMapping("/detail/{id}")
     public ResponseEntity<String> ShopDetail(@PathVariable("id") Long id) throws Exception {
         try {
-        String shopDetail = detailService.ShopDetailList(id);
-        return new ResponseEntity<>(shopDetail, HttpStatus.OK);
+            String shopDetail = detailService.ShopDetailList(id);
+            return new ResponseEntity<>(shopDetail, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
+//    @GetMapping("/review/{id}/{id}")
+//    public ResponseEntity<Page<Review>> PageReview(@PathVariable("id"))
+//
+//}
 
 }
-
