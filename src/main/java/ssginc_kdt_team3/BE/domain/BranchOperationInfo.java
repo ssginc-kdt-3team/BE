32f80bb0cdd3cbbc523e31f11bcbf4728a39032f
imtaegyeong <xorud1305@gmail.com>
2023-05-22 21:12:42 +0900

    admin create branch

--- src/main/java/ssginc_kdt_team3/BE/domain/BranchOperationInfo.java
+++ src/main/java/ssginc_kdt_team3/BE/domain/BranchOperationInfo.java
@@ -1,13 +1,18 @@
 package ssginc_kdt_team3.BE.domain;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class BranchOperationInfo {
 
     @Id
@@ -16,10 +21,10 @@ public class BranchOperationInfo {
     private Long id;
 
     @Column(name = "branch_open_time")
-    private LocalDateTime openTime;
+    private LocalTime openTime;
 
     @Column(name = "branch_close_time")
-    private LocalDateTime closeTime;
+    private LocalTime closeTime;
 
     @Column(name = "branch_open_day")
     private LocalDate openDay;
@@ -27,4 +32,11 @@ public class BranchOperationInfo {
 
     @Column(name = "branch_out_day")
     private LocalDate outDay;
+
+    @Builder
+    public BranchOperationInfo(LocalTime openTime, LocalTime closeTime, LocalDate openDay) {
+        this.openTime = openTime;
+        this.closeTime = closeTime;
+        this.openDay = openDay;
+    }
 }
