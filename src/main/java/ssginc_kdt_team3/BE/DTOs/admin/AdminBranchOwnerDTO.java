ee5b44c46a9c74bcb70c259e3dc4b3139cc07358
syoung0 <affogato128@gmail.com>
2023-05-25 09:13:25 +0900

    Resolve : AdminOwnerBranch_DTO

--- src/main/java/ssginc_kdt_team3/BE/DTOs/admin/AdminBranchOwnerDTO.java
+++ src/main/java/ssginc_kdt_team3/BE/DTOs/admin/AdminBranchOwnerDTO.java
@@ -1,26 +1,32 @@
 package ssginc_kdt_team3.BE.DTOs.admin;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import ssginc_kdt_team3.BE.enums.UserStatus;
 
 
-@AllArgsConstructor
+
 @Data
 public class AdminBranchOwnerDTO {
 
     private Long ownerId;
-
+    private String ownerName;
+    private String ownerPhone;
+    private UserStatus userStatus;
+    private String shopLocation;
+    private String shopName;
     private String branchName;
 
-    private String shopName;
-
-    private String ownerName;
-
-    private String ownerPhone;
-
-    private String shopLocation;
-
+    public AdminBranchOwnerDTO(Long id, String ownerName, String ownerPhone, UserStatus userStatus, String shopName, String shopLocation, String branchName) {
+        this.ownerId = id;
+        this.ownerName = ownerName;
+        this.ownerPhone = ownerPhone;
+        this.userStatus = userStatus;
+        this.shopName = shopName;
+        this.shopLocation = shopLocation;
+        this.branchName = branchName;
+    }
     public AdminBranchOwnerDTO(){
 
     }
+
 }
